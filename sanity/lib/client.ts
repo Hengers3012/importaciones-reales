2fd2ee91dd8fee209b6ba4227d652af80bf32cd8
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2023-09-13 14:56:08 -0400

    Created Products schema file. Added token with editor access.

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,5 @@ export const client = createClient({
 	dataset,
 	projectId,
 	useCdn,
+	token: "skywn46fJT5iDQZuIOK08xTlVnw2yLWeJwuOFbw2NES8Ay7aCKNgm99OOaERPknZTLhxkNaVSnh2WQvPBN2TE1cGyNxs9IlKS9DxGD9gwNBwvRuLjDQDpOau0BkD4KtzxeBLD278hKCR9U454UiZajXwmmO0oSHI50QpxP2gZR6RHTq4SkAX",
 });
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 
+import { product } from "./schemas/product";
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-	types: [],
+	types: [product],
 };
